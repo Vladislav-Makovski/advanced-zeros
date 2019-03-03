df081a094c62217e1c86d6eca93f2e6c5ce5fd4c
Vladislav Makovski <makovskivladislav@mail.ru>
2019-03-03 23:55:07 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let copyBase = base;
+  let result = 0;
+  let divisionResult = 0;
+  let minimum = [];
+  let simpleDividers = [];
+  let divisorDegrees = [];
+  for (let i = 2; i <= base; i++) {
+  if(copyBase === 1){break;}
+  if(copyBase % i === 0){
+   if(simpleDividers.length === 0){
+     simpleDividers.push(i);
+     divisorDegrees[0] = 1;
+     copyBase /= i;
+     i--;
+   }else if(simpleDividers[simpleDividers.length - 1] === i){
+     divisorDegrees[simpleDividers.length-1]++;
+     copyBase /= i;
+     i--;
+   }else{
+     simpleDividers.push(i);
+     divisorDegrees[simpleDividers.length - 1] = 1;
+     copyBase /= i;
+     i--;
+   }
+  }
+}
+for(let j = simpleDividers.length - 1; j >= 0; j--){
+  let temp = simpleDividers[j];
+while(temp <= number){
+divisionResult += Math.floor(number / temp);
+temp *= simpleDividers[j]; 
+}
+minimum[j] = Math.floor(divisionResult/ divisorDegrees[j]);
+divisionResult = 0;
+}
+result = minimum[0];
+for (let i = 1; i < minimum.length; i++) {
+  if(result > minimum[i]){
+    result = minimum[i];
+  }
+}
+return result;
 }
